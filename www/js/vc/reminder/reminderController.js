30838b5023151c7bd50a6a9fb6ea6cc406c5a10c
getlunch.ru <getlup@dedi797.your-server.de>
2016-03-28 17:45:45 +0200

    Updated 18.49 28.03.2016

--- www/js/vc/reminder/reminderController.js
+++ www/js/vc/reminder/reminderController.js
@@ -1,6 +1,6 @@
 define(["app", "js/utilities/picker","js/utilities/forms"], function(app, picker, forms) {
 	var $ = Framework7.$;
-	var weekdays={mon:'monday',tue:'tuesday',wed:'wednesday',thu:'thursday',fri:'friday',sat:'saturday',sun:'sunday'};
+	var weekdays={mon:1,tue:2,wed:3,thu:4,fri:5,sat:6,sun:7};
 	function init(query) {
 		var $input = $('#reminderTime');
 		var initTime=false;
@@ -32,42 +32,34 @@ define(["app", "js/utilities/picker","js/utilities/forms"], function(app, picker
 			var data=forms.serialize(form);
 			var dataArr=forms.serialize(form,'array');			
 			localStorage.setItem('reminderForm',JSON.stringify(dataArr));
-			
-			var user=JSON.parse(localStorage.getItem('User'));
-			if(user)data+='&iuser='+user.id;			
-			var pushRegistrationId=localStorage.getItem('pushRegistrationId');	
-			
-			console.log(dataArr);
-			
 			var every=0;
 			if($.isArray(dataArr.weekly)) every='week';
-			var firstAt=[];
+			//var user=JSON.parse(localStorage.getItem('User'));
+			//if(user)data+='&iuser='+user.id;			
+			//var pushRegistrationId=localStorage.getItem('pushRegistrationId');
+			var date = new Date();
+			var time=dataArr.time.split(':');
 			if($.isArray(dataArr['days[]'])){
-				console.log(firstAt);   
 				for(var i in dataArr['days[]']){
-					firstAt.push(weekdays[dataArr['days[]'][i]]);
+					var d=new Date(date.getFullYear(),date.getMonth(),date.getDate()+weekdays[dataArr['days[]'][i]]-1,time[0],time[1],'00');
+					try{
+						cordova.plugins.notification.local.schedule({
+						    id: i,
+						    title: "GetLunch",
+						    text: "Пора есть!",
+						    at: d,
+						    every: every,
+						    //sound: "file://sounds/reminder.mp3",
+						    icon: "file://i/120pt.png",
+						});
+					}catch(e){console.log(e);}
 				}
-			}
-			firstAt.push(dataArr.time);
-			console.log(firstAt);
-			firstAt=firstAt.join('_');
-			console.log(firstAt);
-			
-			try{
-				cordova.plugins.notification.local.schedule({
-				    //id: 1,
-				    title: "GetLunch",
-				    text: "Пора есть!",
-				    at: firstAt,
-				    every: every,
-				    //sound: "file://sounds/reminder.mp3",
-				    //icon: "http://icons.com/?cal_id=1",
-				    //data: { meetingId:"123#fg8" }
-				});
 				forms.showMessage('Напоминание установлено!', 'success');
-			}catch(e){		
-				console.log(e);
-				//console.log(data);
+			}else{
+				try{
+					cordova.plugins.notification.local.cancel([1,2,3,4,5,6,7], function () {}, scope);
+				}catch(e){console.log(e);}
+				forms.showMessage('Напоминание отключено', 'success');
 			}
 		});
 		$('.b_reminder_checkbox').on('click', function(){
